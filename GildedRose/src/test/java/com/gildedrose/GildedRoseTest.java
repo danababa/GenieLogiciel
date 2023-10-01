f22ed77cddb5a929b4aed14f59933afe161bb58b
Dana El Baba <danaselbaba@gmail.com>
2023-10-01 23:47:55 +0200

    Ajout des tests part2

--- GildedRose/src/test/java/com/gildedrose/GildedRoseTest.java
+++ GildedRose/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -55,23 +55,77 @@ class GildedRoseTest {
   assertEquals("Sulfuras, Hand of Ragnaros", element4.name, "different name");
   }
 
-  // test if (items[i].quality < 50)
   @Test 
-  @DisplayName("Test quality < 50 ")
-  void testLT(){
-  Item element3 = new Item("Aged Brie", 0, 51);
-  GildedRose app = new GildedRose(new Item[] {element3});
+  @DisplayName("Test Sulfuras, Hand of Ragnaros")
+  void testName3(){
+  Item element4 = new Item("Sulfuras, Hand of Ragnaros", -1, 49);
+  GildedRose app = new GildedRose(new Item[] {element4});
   app.updateQuality();
-  assertEquals("Aged Brie", element3.name, "quantity greater than zero");
+  assertEquals("Sulfuras, Hand of Ragnaros", element4.name, "different name");
   }
 
+  // test if (items[i].quality < 50)
+  @Test 
+  @DisplayName("Test quality > 50 if name is Backstage passes to a TAFKAL80ETC concert ")
+  void testLT(){
+  Item element3 = new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49);
+  GildedRose app = new GildedRose(new Item[] {element3});
+  app.updateQuality();
+  assertEquals("Backstage passes to a TAFKAL80ETC concert", element3.name, "quantity greater than fifty");
+  }
+
+  @Test 
+  @DisplayName("Test quality < 50 ")
+  void testGT(){
+  Item element3 = new Item("Backstage passes to a TAFKAL80ETC concert", 10, 51);
+  GildedRose app = new GildedRose(new Item[] {element3});
+  app.updateQuality();
+  assertEquals("Backstage passes to a TAFKAL80ETC concert", element3.name, "quantity greater than fifty");
+  }
+
+  @Test 
+  @DisplayName("Test quality > 50 in all other casses  ")
+  void testLT2(){
+  Item element3 = new Item("Backstage passes to a TAFKAL80ETC concert", 0, 49);
+  GildedRose app = new GildedRose(new Item[] {element3});
+  app.updateQuality();
+  assertEquals("Backstage passes to a TAFKAL80ETC concert", element3.name, "quantity greater than fifty");
+  }
+
+  @Test 
+  @DisplayName("Test quality < 50 ")
+  void testGT2(){
+  Item element3 = new Item("Backstage passes to a TAFKAL80ETC concert", 0, 51);
+  GildedRose app = new GildedRose(new Item[] {element3});
+  app.updateQuality();
+  assertEquals("Backstage passes to a TAFKAL80ETC concert", element3.name, "quantity greater than fifty");
+  }
+
+
+  @Test 
+  @DisplayName("Test quality > 50 in all other casses  ")
+  void testLT3(){
+  Item element3 = new Item("Backstage passes to a TAFKAL80ETC concert", 1, 49);
+  GildedRose app = new GildedRose(new Item[] {element3});
+  app.updateQuality();
+  assertEquals("Backstage passes to a TAFKAL80ETC concert", element3.name, "quantity greater than fifty");
+  }
+
+  @Test 
+  @DisplayName("Test quality < 50 ")
+  void testGT3(){
+  Item element3 = new Item("Backstage passes to a TAFKAL80ETC concert", 1, 51);
+  GildedRose app = new GildedRose(new Item[] {element3});
+  app.updateQuality();
+  assertEquals("Backstage passes to a TAFKAL80ETC concert", element3.name, "quantity greater than fifty");
+  }
   @Test 
   @DisplayName("Test SellIn < 11 ")
   void testSellInLT(){
   Item element3 = new Item("Backstage passes to a TAFKAL80ETC concert", 9, 0);
   GildedRose app = new GildedRose(new Item[] {element3});
   app.updateQuality();
-  assertEquals("Backstage passes to a TAFKAL80ETC concert", element3.name, "quantity greater than zero");
+  assertEquals("Backstage passes to a TAFKAL80ETC concert", element3.name, "SellIn less than eleven");
   }
 
   @Test 
@@ -80,7 +134,7 @@ class GildedRoseTest {
   Item element3 = new Item("Backstage passes to a TAFKAL80ETC concert", 12, 0);
   GildedRose app = new GildedRose(new Item[] {element3});
   app.updateQuality();
-  assertEquals("Backstage passes to a TAFKAL80ETC concert", element3.name, "quantity greater than zero");
+  assertEquals("Backstage passes to a TAFKAL80ETC concert", element3.name, "SellIn greater than eleven");
   }
 
 
